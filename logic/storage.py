d2437e0e18e70cd8ae58fa0baaf37183423c4f51
MAZOUZ ABDERRAHMANE <mazathomazigh@gmail.com>
2026-05-04 14:47:38 +0100

    :storage operation complited

---
+++ logic/storage.py
@@ -0,0 +1,21 @@
+import json
+import os
+
+FICHIER = "data/books.json"
+
+# Charger les livres depuis le fichier
+def load_books():
+    if not os.path.exists(FICHIER):
+        return []
+    with open(FICHIER, "r") as f:
+        data = json.load(f)
+
+    if isinstance(data, dict) and isinstance(data.get("book"), list):
+        return data["book"]
+
+    return data if isinstance(data, list) else []
+
+# Sauvegarder les livres dans le fichier
+def save_books(books):
+    with open(FICHIER, "w") as f:
+        json.dump({"book": books}, f, indent=4)
